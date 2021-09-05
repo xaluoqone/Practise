1c1a2ea656662315385fbf471194752a8a305ecf
xaluoqone <1617321643@qq.com>
2021-09-05 17:21:20 +0800

    project add BiorhythmView

---
+++ app/src/main/java/com/xaluoqone/practise/circle/BiorhythmView.kt
@@ -0,0 +1,8 @@
+package com.xaluoqone.practise.circle
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BiorhythmView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+}
---
+++ app/src/main/java/com/xaluoqone/practise/ex/ScreenEx.kt
@@ -0,0 +1,18 @@
+package com.xaluoqone.practise.ex
+
+import android.content.res.Resources
+import android.util.TypedValue
+
+val Number.dp: Float
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    )
+
+val Number.sp: Float
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    )
