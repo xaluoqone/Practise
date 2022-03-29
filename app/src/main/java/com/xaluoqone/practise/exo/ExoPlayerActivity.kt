23159519a6b5d761fff8372121b624191ca073f0
xaluoqone <1617321643@qq.com>
2022-03-29 23:14:28 +0800

    ExoPlayerActivity adapter status bar

--- app/src/main/java/com/xaluoqone/practise/exo/ExoPlayerActivity.kt
+++ app/src/main/java/com/xaluoqone/practise/exo/ExoPlayerActivity.kt
@@ -2,9 +2,12 @@ package com.xaluoqone.practise.exo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.xaluoqone.practise.widget.linear
 
-class ExoPlayerActivity:AppCompatActivity() {
+class ExoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(
@@ -12,5 +15,18 @@ class ExoPlayerActivity:AppCompatActivity() {
 
             }
         )
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        ViewCompat.getWindowInsetsController(window.decorView)?.apply {
+            isAppearanceLightStatusBars = true
+        }
+
+        window.decorView.setOnApplyWindowInsetsListener { _, windowInsets ->
+            val insets = WindowInsetsCompat.toWindowInsetsCompat(windowInsets)
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            window.decorView.setPadding(0, statusBarInsets.top, 0, 0)
+            windowInsets
+        }
     }
 }
