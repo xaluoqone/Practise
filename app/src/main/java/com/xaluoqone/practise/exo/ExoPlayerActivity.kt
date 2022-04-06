32af43d772c21e22330315204cd35d74c105469c
xaluoqone <1617321643@qq.com>
2022-04-06 15:04:35 +0800

    更新界面UI

--- app/src/main/java/com/xaluoqone/practise/exo/ExoPlayerActivity.kt
+++ app/src/main/java/com/xaluoqone/practise/exo/ExoPlayerActivity.kt
@@ -2,15 +2,40 @@ package com.xaluoqone.practise.exo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.xaluoqone.practise.widget.linear
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.xaluoqone.practise.widget.*
 
-class ExoPlayerActivity:AppCompatActivity() {
+class ExoPlayerActivity : AppCompatActivity() {
+    private val player by lazy {
+        ExoPlayer.Builder(this).build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(
             linear {
-
+                orientation = vertical
+                addView(StyledPlayerView(this@ExoPlayerActivity).apply {
+                    layoutParams = layoutParams {
+                        width = match
+                        height = wrap
+                    }
+                    player = this@ExoPlayerActivity.player
+                })
             }
         )
+
+        val mediaItem = MediaItem.fromUri("https://xaluoqone.com/files/video/benxi.mp4")
+
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
     }
 }
