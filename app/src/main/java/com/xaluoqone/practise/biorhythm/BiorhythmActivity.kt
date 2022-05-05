2ce61a81e4bffde6ffc2cd77b90760e1425d9192
xaluoqone <1617321643@qq.com>
2022-05-05 10:10:37 +0800

    新适配透明状态栏

--- app/src/main/java/com/xaluoqone/practise/biorhythm/BiorhythmActivity.kt
+++ app/src/main/java/com/xaluoqone/practise/biorhythm/BiorhythmActivity.kt
@@ -3,24 +3,27 @@ package com.xaluoqone.practise.biorhythm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.setMargins
+import androidx.core.view.updatePadding
 import com.xaluoqone.practise.R
-import com.xaluoqone.practise.ex.applyStatusBarPadding
-import com.xaluoqone.practise.ex.dpi
-import com.xaluoqone.practise.ex.setStatusBarTextColorIsLight
-import com.xaluoqone.practise.ex.setStatusBarTransparent
+import com.xaluoqone.practise.ex.*
 import com.xaluoqone.practise.widget.*
 
 class BiorhythmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStatusBarTransparent()
-        setStatusBarTextColorIsLight(true)
-        findViewById<FrameLayout>(android.R.id.content).applyStatusBarPadding()
+        configSystemBar {
+            statusBarBlackFont = true
+        }
         setContentView(
             linear {
                 orientation = vertical
+                doOnApplyWindowInsets { insets, _, _ ->
+                    val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+                    updatePadding(top = statusBarInsets.top)
+                }
                 val biorhythmView = BiorhythmView(this@BiorhythmActivity).apply {
                     layoutParams {
                         width = match
