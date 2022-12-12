a70db9de6783cd7fe0771cfcfee161cd0220b7fa
xaluoqone <1617321643@qq.com>
2022-12-12 16:07:47 +0800

    fix bug

--- app/src/main/java/com/xaluoqone/practise/database/dao/TestInfoDao.kt
+++ app/src/main/java/com/xaluoqone/practise/database/dao/TestInfoDao.kt
@@ -2,6 +2,7 @@ package com.xaluoqone.practise.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.xaluoqone.practise.database.table.TestInfo
 import kotlinx.coroutines.flow.Flow
@@ -12,7 +13,7 @@ interface TestInfoDao {
     @Query("select * from test_info")
     fun queryAllTestInfo(): Flow<List<TestInfo>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTestInfoList(infoList: List<TestInfo>)
 
     @Query("delete from test_info where id not in(:ids)")
